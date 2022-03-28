817150e35266c9cff05a2908d877caf0081a8cdc
Chicle <chicle@protonmail.com>
2022-03-28 06:42:52 +0200

    feat: init command Only checks if the file resources.json exists in the working directory

---
+++ cmd/init.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/Eschiclers/fiverm/pkg/workingdirectory"
+	"github.com/spf13/cobra"
+)
+
+// initCmd represents the init command
+var initCmd = &cobra.Command{
+	Use:   "init",
+	Short: "Create a fiverm.json file",
+	Long:  `Create a fiverm.json file`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if _, err := os.Stat(workingdirectory.GetWorkingDirectory() + string(os.PathSeparator) + "resources.json"); os.IsNotExist(err) {
+			fmt.Println("Creating resources.json file")
+		} else {
+			fmt.Println("resource.json file already exists")
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// initCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// initCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}

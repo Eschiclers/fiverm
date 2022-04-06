b27af21844bb9cc77bb18ffb99f11ac1dfd08066
Chicle <chicle@protonmail.com>
2022-04-06 06:34:21 +0200

    add: Remove command

---
+++ cmd/remove.go
@@ -0,0 +1,25 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// removeCmd represents the remove command
+var removeCmd = &cobra.Command{
+	Use:   "remove [resource]",
+	Short: "Remove a resource",
+	Long:  `Remove a resource`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("remove called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(removeCmd)
+}
